a226d9c7a8f7b2c82e702c5e0a79eb817ad5a17d
FernandoNakasone <fernandonakasone040@gmail.com>
2026-03-25 10:33:47 -0300

    feat: add entities, import.sql, application-test

---
+++ ms-pedidos/src/main/resources/imports.sql
@@ -0,0 +1,6 @@
+insert into tb_pedido(data,valor_total,cpf,nome,status) values('2025-11-25',540.00,'12345678936','Jon Snow','CRIADO');
+insert into TB_PEDIDO(data,valor_total,cpf,nome,status) values('2026-01-25',3599.00,'82236545693','Arya Stark','CRIADO');
+
+insert into tb_item_do_pedido(preco_unitario,quantidade,pedido_id,descricao) values(250.00,2,1,'Mouse sem fio Microsoft');
+insert into tb_item_do_pedido(preco_unitario,quantidade,pedido_id,descricao) values(290.00,1,1,'Teclado sem fio Microsoft');
+insert into tb_item_do_pedido(preco_unitario,quantidade,pedido_id,descricao) values(3599.00,1,2,'Smart TV LG LED');
